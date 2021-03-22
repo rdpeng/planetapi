0085e69e30c1eb273063cefc8a341a3c4acf0454
Roger D. Peng <rdpeng@jhu.edu>
2021-03-22 16:03:28 -0400

    More plotting func

--- R/api.R
+++ R/api.R
@@ -47,20 +47,26 @@ plnt_quota <- function() {
 #' Build Search Query
 #'
 #' Create JSON for search query
-plnt_build_search <- function() {
-
+asset_filter <- function(asset_type) {
+        list(type = "AssetFilter",
+             config = as.list(asset_type))
 }
 
 date_range_filter <- function(start, end) {
-        obj <- list(type = "DateRangeFilter",
-                    field_name = "acquired",
-                    config = list(
-                            gte = paste0(start, "T00:00:00.000Z"),
-                            lte = paste0(end, "T00:00:00.000Z")
-                    ))
-        obj
+        list(type = "DateRangeFilter",
+             field_name = "acquired",
+             config = list(
+                     gte = paste0(start, "T00:00:00.000Z"),
+                     lte = paste0(end, "T00:00:00.000Z")
+             ))
 }
 
+geometry_filter <- function(aoi_geojson) {
+        json <- read_json(aoi_geojson)
+        list(type = "GeometryFilter",
+             field_name = "geometry",
+             config = json$features[[1]]$geometry)
+}
 
 #' Search Planet API
 #'
@@ -76,6 +82,7 @@ date_range_filter <- function(start, end) {
 #' @export
 #'
 plnt_search <- function(details_json) {
+        auth <- get_auth()
         search_string <- file.path(searchurl,
                                    URLencode(paste0("quick-search",
                                                     "?_sort=acquired asc",
@@ -99,6 +106,7 @@ plnt_search <- function(details_json) {
 #' @export
 #'
 plnt_order <- function(details_json) {
+        auth <- get_auth()
         r <- POST(orderurl,
                   auth,
                   body = upload_file(details_json))
@@ -116,18 +124,48 @@ plnt_order <- function(details_json) {
 #'
 #' @description Combine values in red (band3), green (band2), and blue (band1) channels to create a "color" image
 #'
-#' @import rgdal
 #' @export
 #'
 plnt_plot_visual <- function(x, pch = 15, ...) {
         col <- with(x@data, rgb(band3, band2, band1, maxColorValue = 255))
-        gr <- expand.grid(x = 1:203, y = 116:1)
+        dxy <- x@grid@cells.dim
+        gr <- expand.grid(x = 1:dxy[1], y = dxy[2]:1)
         gr$col <- col
         with(gr, plot(x, y, col = col, pch = pch,
                       asp = 1, ...))
 }
 
 
+normalize_band <- function(x, max_value = 255) {
+        x <- (x - min(x, na.rm = TRUE)) / max(x, na.rm = TRUE)
+        x <- x * max_value
+        x[is.na(x)] <- 0
+        x
+}
+
+#' Plot Image
+#'
+#' Plot Any Satellite Image via RGB
+#'
+#' @param x a GDAL object
+#' @param pch pch for plot (default 15)
+#' @param ... other parameters passed to \code{plot}
+#'
+#' @description Normalize color bands in RGB and then combine values in red (band3), green (band2), and blue (band1) channels to create a "color" image
+#'
+#' @export
+#'
+plnt_plot_any <- function(x, pch = 15, ...) {
+        x@data$band3 <- normalize_band(x@data$band3)
+        x@data$band2 <- normalize_band(x@data$band2)
+        x@data$band1 <- normalize_band(x@data$band1)
+        col <- with(x@data, rgb(band3, band2, band1, maxColorValue = 255))
+        dxy <- x@grid@cells.dim
+        gr <- expand.grid(x = 1:dxy[1], y = dxy[2]:1)
+        gr$col <- col
+        with(gr, plot(x, y, col = col, pch = pch,
+                      asp = 1, ...))
+}
 
 
 
